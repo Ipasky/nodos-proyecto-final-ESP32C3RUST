5f79314b158c24906e607c3f4f2336f7848b7f5e
mdelcos-pixel <mdelcos@ucm.es>
2026-01-02 11:51:48 +0100

    Update P1.c

--- P1_Introducción_al_entorno/P1.c
+++ P1_Introducción_al_entorno/P1.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h> 
+#include "driver/gpio.h" 
+#include "freertos/FreeRTOS.h"
+#include "driver/ledc.h"
+#define LED GPIO_NUM_7
+void app_main(void) {
+ gpio_reset_pin(LED);    
+  gpio_set_direction(LED, GPIO_MODE_OUTPUT);    
+  while (1) {        
+    gpio_set_level(LED, 0);        
+    vTaskDelay(1000 / portTICK_PERIOD_MS);        
+    gpio_set_level(LED, 1);        
+    vTaskDelay(1000 / portTICK_PERIOD_MS);    
+    } 
+}
