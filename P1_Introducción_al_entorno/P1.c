76ad793f36023c30584b05a3fdf7f9521614d89e
mdelcos-pixel <mdelcos@ucm.es>
2026-01-05 21:12:39 +0100

    Update P1.c

--- P1_Introducción_al_entorno/P1.c
+++ P1_Introducción_al_entorno/P1.c
@@ -1,15 +1,29 @@
-#include <stdio.h> 
-#include "driver/gpio.h" 
-#include "freertos/FreeRTOS.h"
-#include "driver/ledc.h"
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "esp32c3/rom/ets_sys.h"  // Para ets_delay_us()
+
 #define LED GPIO_NUM_7
-void app_main(void) {
- gpio_reset_pin(LED);    
-  gpio_set_direction(LED, GPIO_MODE_OUTPUT);    
-  while (1) {        
-    gpio_set_level(LED, 0);        
-    vTaskDelay(1000 / portTICK_PERIOD_MS);        
-    gpio_set_level(LED, 1);        
-    vTaskDelay(1000 / portTICK_PERIOD_MS);    
-    } 
+
+void app_main(void)
+{
+    // Inicializamos el pin como salida
+    gpio_reset_pin(LED);
+    gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+
+    while (1)
+    {
+        // LED apagado
+        gpio_set_level(LED, 0);
+
+        // Delay de 1 segundo (1000000 microsegundos)
+        ets_delay_us(1000000);
+
+        // LED encendido
+        gpio_set_level(LED, 1);
+
+        // Delay de 1 segundo
+        ets_delay_us(1000000);
+    }
 }
+
+
