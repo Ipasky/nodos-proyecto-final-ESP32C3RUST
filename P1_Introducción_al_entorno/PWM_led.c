9235dc5ed2b62933e0e3c5ede0ddad46e3945ca0
mdelcos-pixel <mdelcos@ucm.es>
2026-01-05 21:28:16 +0100

    Update PWM_led.c

--- P1_Introducción_al_entorno/PWM_led.c
+++ P1_Introducción_al_entorno/PWM_led.c
@@ -1,37 +1,24 @@
-#include <stdio.h>
-#include <stdio.h> 
-#include "driver/gpio.h" 
-#include "freertos/FreeRTOS.h" 
-#include "esp_rom_sys.h" 
-#include <sys/time.h> 
-#include "driver/ledc.h" 
-void delay_ms(int t) { 
-    vTaskDelay(t / portTICK_PERIOD_MS); 
-} 
-void PWMconfigLow(int gpio, int chan, int timer, int res, int freq, float duty) { 
-    ledc_timer_config_t ledc_timer = { .speed_mode = LEDC_LOW_SPEED_MODE, .clk_cfg = LEDC_APB_CLK }; 
-    ledc_timer.timer_num = timer; 
-    ledc_timer.duty_resolution = res; 
-    ledc_timer.freq_hz = freq;
-    ledc_channel_config_t ledc_channel = { .speed_mode = 
-        LEDC_LOW_SPEED_MODE, 
-        .hpoint = 0, 
-        .intr_type = LEDC_INTR_DISABLE, };
+#include "driver/gpio.h"
+#include "esp32c3/rom/ets_sys.h"
 
-     ledc_channel.channel = chan;  
-     ledc_channel.timer_sel = timer;  
-     ledc_channel.gpio_num = gpio;  
-     ledc_channel.duty = ((float)(2 << (res - 1))) * duty;  
-     ledc_timer_config(&ledc_timer);  
-     ledc_channel_config(&ledc_channel); 
+#define LED 7
+
+void app_main(void) {
+    gpio_reset_pin(LED);
+    gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        // PWM por software, barrido de brillo
+        for (int duty = 0; duty <= 255; duty++) {
+            int on_time = (2000 * duty) / 255;  // periodo 2 ms
+            int off_time = 2000 - on_time;
+
+            gpio_set_level(LED, 1);
+            ets_delay_us(on_time);
+
+            gpio_set_level(LED, 0);
+            ets_delay_us(off_time);
+        }
     }
-void app_main(void) {  
-    while (true)  {    
-        PWMconfigLow(7, 0, 0, 8, 2000, 0.5);    
-        for (int d = 0; d <= 256; d++)    {      
-            ledc_set_duty(LEDC_LOW_SPEED_MODE, 0, d);      
-            ledc_update_duty(LEDC_LOW_SPEED_MODE, 0);      
-            delay_ms(10);    
-        }  
-    } 
 }
+
