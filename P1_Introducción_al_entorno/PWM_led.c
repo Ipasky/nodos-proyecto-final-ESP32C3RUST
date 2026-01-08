c335ae08bfc8b7917f6893435e27ea006b4b44ca
mdelcos-pixel <mdelcos@ucm.es>
2026-01-08 17:45:12 +0100

    Update PWM_led.c

--- P1_Introducción_al_entorno/PWM_led.c
+++ P1_Introducción_al_entorno/PWM_led.c
@@ -6,9 +6,9 @@
 #define GPIO_OUT_W1TC    (GPIO_BASE + 0x0C)
 #define GPIO_ENABLE_W1TS (GPIO_BASE + 0x20)
 
-#define LED_PIN 2       // LED seguro en DevKit ESP32-C3
+#define LED_PIN 2       
 #define PWM_RES 256     // resolución 8 bits
-#define PWM_DELAY_US 50 // microsegundos por paso (ajusta frecuencia)
+#define PWM_DELAY_US 50 // microsegundos por paso 
 
 // registros GPIO
 volatile uint32_t* gpio_out_w1ts = (volatile uint32_t*) GPIO_OUT_W1TS;
@@ -20,7 +20,7 @@ void app_main(void) {
     *gpio_enable_w1ts = (1 << LED_PIN);
 
     while (1) {
-        // Subir brillo (0 → 255)
+        //sube brillo
         for (int duty = 0; duty < PWM_RES; duty++) {
             for (int i = 0; i < PWM_RES; i++) {
                 if (i < duty)
@@ -31,7 +31,7 @@ void app_main(void) {
             }
         }
 
-        // Bajar brillo (255 → 0)
+        // baja brillo
         for (int duty = PWM_RES - 1; duty >= 0; duty--) {
             for (int i = 0; i < PWM_RES; i++) {
                 if (i < duty)
